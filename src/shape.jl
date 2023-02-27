e4334b83c47f6da1fe80f375c0812f2cdc98eb85
Princic-1837592 <andreapri.99@gmail.com>
2023-02-27 15:17:27 +0100

    applied alignment

--- src/shape.jl
+++ src/shape.jl
@@ -10,17 +10,16 @@ module Shape
 using ..Math: Vec2i, Vec3f, Vec4f, Vec3i, Vec4i, Vec2f
 
 struct ShapeData
-    points::Array{Int32,1}
-    lines::Array{Vec2i,1}
-    triangles::Array{Vec3i,1}
-    quads::Array{Vec4i,1}
-
-    positions::Array{Vec3f,1}
-    normals::Array{Vec3f,1}
-    texcoords::Array{Vec2f,1}
-    colors::Array{Vec4f,1}
-    radius::Array{Float32,1}
-    tangents::Array{Vec4f,1}
+    points    :: Array{Int32,1}
+    lines     :: Array{Vec2i,1}
+    triangles :: Array{Vec3i,1}
+    quads     :: Array{Vec4i,1}
+    positions :: Array{Vec3f,1}
+    normals   :: Array{Vec3f,1}
+    texcoords :: Array{Vec2f,1}
+    colors    :: Array{Vec4f,1}
+    radius    :: Array{Float32,1}
+    tangents  :: Array{Vec4f,1}
 end
 
 end
