0e1e73696cf63d5bb95a22027c6cc69c8c8d4b8e
Princic-1837592 <andreapri.99@gmail.com>
2023-02-27 12:49:32 +0100

    declared some structs and functions only to make it compile

--- src/shape.jl
+++ src/shape.jl
@@ -5,11 +5,22 @@ shape:
 - Date: 2023-01-03
 =#
 
-using LinearAlgebra
-using StaticArrays
+module Shape
 
-#todo capire quale tipo di vettore usare
-mutable struct BvhTree
-    nodes
-    primitives
+using ..Math: Vec2i, Vec3f, Vec4f, Vec3i, Vec4i, Vec2f
+
+struct ShapeData
+    points::Array{Int32,1}
+    lines::Array{Vec2i,1}
+    triangles::Array{Vec3i,1}
+    quads::Array{Vec4i,1}
+
+    positions::Array{Vec3f,1}
+    normals::Array{Vec3f,1}
+    texcoords::Array{Vec2f,1}
+    colors::Array{Vec4f,1}
+    radius::Array{Float32,1}
+    tangents::Array{Vec4f,1}
+end
+
 end
