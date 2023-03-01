37f97ae74b06ac3053a68adaca9a6180442db005
Princic-1837592 <andreapri.99@gmail.com>
2023-03-01 10:16:40 +0100

    defined bvh structures

--- src/bvh.jl
+++ src/bvh.jl
@@ -6,16 +6,42 @@ bvh:
 =#
 
 module Bvh
-#
-# mutable struct ShapeBvh
-#     bvh::BvhTree
-# end
-#
-# mutable struct SceneBvh{N}
-#     bvh::BvhTree
-#     shapes::SVector{N,ShapeBvh}
-# end
-#
-# #todo aggiungere tipo di scene da scene.jl
-function make_scene_bvh(scene) end
+
+using ..Scene: SceneData
+using ..Math: Vec3f
+
+struct Bbox3f
+    min::Vec3f
+    max::Vec3f
+
+    Bbox3f() = new(
+        Vec3f(typemax(Float32), typemax(Float32), typemax(Float32)),
+        Vec3f(typemin(Float32), typemin(Float32), typemin(Float32)),
+    )
+end
+
+struct BvhNode
+    bbox     :: Bbox3f
+    start    :: Int32
+    num      :: Int16
+    axis     :: Int8
+    internal :: Bool
+end
+
+struct BvhTree
+    nodes      :: Array{BvhNode,1}
+    primitives :: Array{Int32,1}
+end
+
+struct ShapeBvh
+    bvh::BvhTree
+end
+
+struct SceneBvh
+    bvh    :: BvhTree
+    shapes :: Array{ShapeBvh,1}
+end
+
+function make_scene_bvh(scene::SceneData, params) end
+
 end
